20d5c054ceeab328491f92b1061bd85bc55b27dd
n3f0 <nefo424@gmail.com>
2022-09-13 16:33:49 +0700

    update menu

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,57 +1,62 @@
-import React from 'react'
-import './navbar.css'
-const Navbar=(props)=>{
-    return (
-        <nav className='menu'>
-           <ul className="nav">
+import React from "react";
+import "./navbar.css";
+const Navbar = (props) => {
+  return (
+    <nav className="top-nav">
+        <a href="/">Home</a>
+        <ul className="nav-group">
             <li><a href="">Persiapan</a>
                 <ul>
                     <li><a href="">Rekanan</a></li>
                     <li><a href="">Kontrak</a></li>
                     <li><a href="">Adendum</a></li>
-                    <li><a href="">BAST Barang dan Jasa</a></li>
-                    <li><a href="">BAST Belanja Modal</a></li>
+                    <li><a href="">Berita Acara Barang Jasa</a></li>
+                    <li><a href="">Berita Acara Belanja Modal</a></li>
                 </ul>
             </li>
-            <li><a href="">Penerimaan</a>
+            <li><a href="">Penatausahaan Penerimaan</a>
                 <ul>
                     <li><a href="">SKP/SKR</a></li>
-                    <li><a href="">Tanda Bukti Penerimaan</a>
+                    <li><a href="">TBP</a>
                         <ul>
-                            <li><a href="">TBP dengan Penetapan</a></li>
-                            <li><a href="">TBP Tanpa Penetapan</a></li>
+                            <li><a href="">Dengan Penetapan</a></li>
+                            <li><a href="">Tanpa Penetapan</a></li>
                         </ul>
                     </li>
-                    <li><a href="">Surat Tanda Setoran</a>
+                    <li><a href="">STS</a>
                         <ul>
-                            <li><a href="">Tanpa Penetapan</a></li>
                             <li><a href="">Dengan Penetapan</a></li>
-                            <li><a href="">RKUD</a></li>
+                            <li><a href="">Tanpa Penetapan</a></li>
                         </ul>
                     </li>
                     <li><a href="">BKU Bendahara Penerimaan</a></li>
+
+
                 </ul>
             </li>
-            <li><a href="">Pengeluaran</a>
+            <li><a href="">Penatausahaan Pengeluaran</a>
                 <ul>
                     <li><a href="">SPP</a>
                         <ul>
-                            <li><a href="">UP</a></li>
+                            <li><a href="">Uang Persediaan</a></li>
                             <li><a href="">LS</a></li>
                             <li><a href="">GU/TU</a></li>
                         </ul>
                     </li>
                     <li><a href="">SPM</a></li>
                     <li><a href="">Tanda Bukti Pengeluaran (TBP)</a></li>
+                    <li><a href="">Pajak UP/GU/TU</a></li>
+                    <li><a href="">Pelimpahan Uang</a></li>
                     <li><a href="">Pengembalian Belanja</a></li>
                     <li><a href="">BKU Bendahara Pengeluaran</a></li>
                     <li><a href="">Pertanggung Jawaban UP/GU/TU</a></li>
                 </ul>
             </li>
             <li><a href="">Cetak</a></li>
-           </ul>
-        </nav>
-    )
-}
+        </ul>
+        <a href="/logout">Logout</a>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
