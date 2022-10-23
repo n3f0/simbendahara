b35b54f9bfc3597d1412029872409fc658c7f9ba
n3f0 <nefo424@gmail.com>
2022-10-23 10:53:51 +0700

    perbaikan warna

--- src/component/pages/rekanan.jsx
+++ src/component/pages/rekanan.jsx
@@ -1,19 +1,94 @@
 import React from 'react'
+import { useState } from 'react';
 import './rekanan.css'
 
 const Rekanan=(props)=>{
 
+    const data=[
+        {
+        kdp3:'0001',
+        nmp3:'PT. Mencari Berkah',
+        norek:'200125322343',
+        npwp:'00345324030003',
+        jenis:'Perdagangan',
+        alamat:'Jl. Pendopo Raya No. 53 Padang'
+        },
+        {
+        kdp3:'0002',
+        nmp3:'Karang Taruna Balai Gadang',
+        norek:'200125322341',
+        npwp:'0034532403082',
+        jenis:'LSM',
+        alamat:'Koto Tangah'
+        },
+        {
+        kdp3:'0001',
+        nmp3:'PT. Mencari Berkah',
+        norek:'200125322343',
+        npwp:'00345324030003',
+        jenis:'Perdagangan',
+        alamat:'Jl. Pendopo Raya No. 53 Padang'
+        },
+        {
+        kdp3:'0001',
+        nmp3:'PT. Mencari Berkah',
+        norek:'200125322343',
+        npwp:'00345324030003',
+        jenis:'Perdagangan',
+        alamat:'Jl. Pendopo Raya No. 53 Padang'
+        },
+        {
+        kdp3:'0001',
+        nmp3:'PT. Mencari Berkah',
+        norek:'200125322343',
+        npwp:'00345324030003',
+        jenis:'Perdagangan',
+        alamat:'Jl. Pendopo Raya No. 53 Padang'
+        },
+        {
+        kdp3:'0001',
+        nmp3:'PT. Mencari Berkah',
+        norek:'200125322343',
+        npwp:'00345324030003',
+        jenis:'Perdagangan',
+        alamat:'Jl. Pendopo Raya No. 53 Padang'
+        },
+        {
+        kdp3:'0001',
+        nmp3:'PT. Mencari Berkah',
+        norek:'200125322343',
+        npwp:'00345324030003',
+        jenis:'Perdagangan',
+        alamat:'Jl. Pendopo Raya No. 53 Padang'
+        },
+        {
+        kdp3:'0001',
+        nmp3:'PT. Mencari Berkah',
+        norek:'200125322343',
+        npwp:'00345324030003',
+        jenis:'Perdagangan',
+        alamat:'Jl. Pendopo Raya No. 53 Padang'
+        },
+        {
+        kdp3:'0001',
+        nmp3:'PT. Mencari Berkah',
+        norek:'200125322343',
+        npwp:'00345324030003',
+        jenis:'Perdagangan',
+        alamat:'Jl. Pendopo Raya No. 53 Padang'
+        },
+        {
+        kdp3:'0001',
+        nmp3:'PT. Mencari Berkah',
+        norek:'200125322343',
+        npwp:'00345324030003',
+        jenis:'Perdagangan',
+        alamat:'Jl. Pendopo Raya No. 53 Padang'
+        },
+    ]
     return(
         <section className="content">
             <h4>Rekanan</h4>
-            <div className="pilihan">
-                <div className="form-group">
-                    <label htmlFor="kdunit">SKPD/Sub Unit</label>
-                    <input type="text" className='c1' name="kdunit" id="kdunit" disabled />
-                    <button className='btnlookup' onClick={()=>{alert('testing')}}>Cari</button>
-                    <input type="text" className="c2" name="nmunit" id="nmunit" disabled />
-                </div>
-            </div>
             <div className="row">
                 <table className="table">
                     <thead>
@@ -27,6 +102,21 @@ const Rekanan=(props)=>{
                             <th>Action</th>
                         </tr>
                     </thead>
+                    <tbody>
+                        {data.map((listValue,index)=>{
+                            return(
+                                <tr>
+                                    <td>{listValue.kdp3}</td>
+                                    <td>{listValue.nmp3}</td>
+                                    <td>{listValue.norek}</td>
+                                    <td>{listValue.npwp}</td>
+                                    <td>{listValue.jenis}</td>
+                                    <td>{listValue.alamat}</td>
+                                    <td></td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
                 </table>
             </div>
         </section>
